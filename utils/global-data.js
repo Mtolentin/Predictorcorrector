76d8d2d40dd262a41e1812752ef541cd837d5be1
Matthew Tolentin <mattca701@gmail.com>
2025-08-13 22:38:42 -0700

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Home of the Wolves';
+    : 'Algebra 2 with Mr. Tolentin';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Algebra 2 with Mr. Tolentin';
+    : 'Wolfpack, welcome';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'San Ramon Valley High School';
